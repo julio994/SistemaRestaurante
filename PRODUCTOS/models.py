c3e95e8a7ec3dc2c34fb8db79cceb274d293fd0d
“julio994” <julio994@utp.edu.co>
2024-04-05 15:01:35 -0500

    agraga gitignore

---
+++ PRODUCTOS/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
